2a1334692b936c98330999e7456b246ceebc06e9
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-06-01 20:49:30 +0330

    optimize , fix bug

--- detail.py
+++ detail.py
@@ -16,7 +16,8 @@ host="127.0.0.1"
 database = "marzban"
 type_of_get_usage = "api_marzban" # "mysql" or "api_marzban" 
 Authorization_api = " "  # Example = "Bearer ey###########"
-marzban_pane_url = "many.wikmgg.ir:2096" # Example = "sub.panel.com:2096"
+marzban_pane_url = " " # Example = "sub.panel.com:2096"
+ignore_urls = ["1.1.1.1"  , "mtalk.google.com" , "android.apis.google.com" , "dns.google" , "8.8.8.8" , "gstatic" , "10.10." , "1.0.0.1" , "8.8.4.4" , "cloudflare"]
 ############################################################
 
 path_log = f"{path}access.log"
